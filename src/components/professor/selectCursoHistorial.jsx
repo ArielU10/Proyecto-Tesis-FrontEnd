ff26f7a962fa667c5040f27e0a4d3acba2ea2a7c
StalinVinueza <csvinueza@puce.edu.ec>
2025-06-27 11:57:22 -0500

    correcion modal historial incidentes

--- src/components/professor/selectCursoHistorial.jsx
+++ src/components/professor/selectCursoHistorial.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../styles/professor/modalHistorial.css";
 
-const SelectCursoHistorial = ({ courses, selectedCourse, onCourseChange }) => (
-  <div className="select-curso-container">
-    <label>Seleccione un curso:</label>
-    <select value={selectedCourse} onChange={(e) => onCourseChange(e.target.value)}>
-      <option value="">Seleccione...</option>
-      {courses.map(course => (
-        <option key={course.id_course} value={course.id_course}>
-          {course.courseName}
-        </option>
-      ))}
-    </select>
-  </div>
-);
+const SelectCursoHistorial = ({ courses, selectedCourse, onCourseChange }) => {
+  useEffect(() => {
+    console.log("Cursos recibidos:", courses);
+  }, [courses]);
+
+  return (
+    <div className="select-curso-container">
+      <label>Seleccione un curso:</label>
+      <select
+        value={selectedCourse}
+        onChange={(e) => onCourseChange(e.target.value)}
+      >
+        <option value="">Seleccione...</option>
+        {courses.map((item, index) => (
+          <option
+            key={item.course?.id_course || index}
+            value={item.course?.id_course}
+          >
+            {item.course?.courseName || `Curso sin nombre (${index + 1})`}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
 export default SelectCursoHistorial;
